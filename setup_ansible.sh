0eb3149f5700aed410f96b8cc14975b00ee00722
Federico Vagnini <vagnini@it.ibm.com>
2024-05-10 11:17:32 +0200

    missing community.general.dig install

--- setup_ansible.sh
+++ setup_ansible.sh
@@ -1,2 +1,2 @@
 sudo dnf -y install ansible-core libxml2-devel libxslt-devel gcc python3-devel python3-netaddr
-ansible-galaxy collection install ibm.power_hmc ansible.utils ansible.posix
+ansible-galaxy collection install ibm.power_hmc ansible.utils ansible.posix community.general.dig
